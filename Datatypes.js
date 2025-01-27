f650c8f2aa5413af757d5969309a3e52602f362d
Mrigendra Prasad <mrigendraprasad69@gmail.com>
2025-01-27 13:36:04 +0530

    Datatypes in Javascript

---
+++ Datatypes.js
@@ -0,0 +1,15 @@
+// Datatypes in JavaScript
+
+/*
+ 1. Number
+ 2. String
+ 3. Boolean
+ 4. Undefined
+ 5. Null
+
+ These are some available datatypes in JavaScript.
+ */
+
+ let num1 = 23;
+
+console.log(typeof num1)
